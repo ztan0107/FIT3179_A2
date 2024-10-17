5dc7e1c8ec4b7d07c7a626299c67acbcf3adc1bb
ztan0107 <101960115+ztan0107@users.noreply.github.com>
2024-10-18 02:05:23 +0800

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -16,3 +16,9 @@ vegaEmbed("#pie_chart", vg_2).then(function(result) {
 // Access the Vega view instance
 (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_3 = "js/people_affected_by_disasters_2020.vg.json";
+vegaEmbed("#map1", vg_3).then(function(result) {
+// Access the Vega view instance 
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
