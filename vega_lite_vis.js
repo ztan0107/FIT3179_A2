c9cc196e9902eba4540453e1e6d16b7d742f5c96
ztan0107 <101960115+ztan0107@users.noreply.github.com>
2024-10-18 02:13:39 +0800

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -8,13 +8,13 @@
 var vg_1 = "js/natural_disasters_contributes_to_death.vg.json";
 vegaEmbed("#line_chart", vg_1).then(function(result) {
 // Access the Vega view instance
-(https://vega.github.io/vega/docs/api/view/) as result.view
+//(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "js/natural_disasters_contributes_to_death.vg.json";
 vegaEmbed("#pie_chart", vg_2).then(function(result) {
 // Access the Vega view instance
-(https://vega.github.io/vega/docs/api/view/) as result.view
+//(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "js/people_affected_by_disasters_2020.vg.json";
